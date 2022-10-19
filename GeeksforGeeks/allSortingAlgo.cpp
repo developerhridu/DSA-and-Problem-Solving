2ab9eb0f536b716ab50c5f15811e0edd3e219ec4
MR Hridu <developerhridu@gmail.com>
2022-10-19 22:48:12 +0600

    all sorting algo

--- GeeksforGeeks/allSortingAlgo.cpp
+++ GeeksforGeeks/allSortingAlgo.cpp
@@ -1,5 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already
+        // in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
+void merge(int *arr, int s, int e)
+{
+
+    int mid = (s + e) / 2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    // declearing dynamic array
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    // copy values
+    int mainArrayIndex = s;
+    for (int i = 0; i < len1; i++)
+    {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid + 1;
+    for (int i = 0; i < len2; i++)
+    {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    // merge 2 sorted arrays
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while (index1 < len1 && index2 < len2)
+    {
+        if (first[index1] < second[index2])
+        {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else
+        {
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }
+
+    while (index1 < len1)
+    {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while (index2 < len2)
+    {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    // deleting dynamic array
+    // make sure you delete dynamic array after the fuction otherwise memory problem will arise. memory will be free if u delete it
+    delete[] first;
+    delete[] second;
+}
+
+void mergeSort(int *arr, int s, int e)
+{
+
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+
+    int mid = (s + e) / 2;
+
+    // left part sort karna h
+    mergeSort(arr, s, mid);
+
+    // right part sort karna h
+    mergeSort(arr, mid + 1, e);
+
+    // merge
+    merge(arr, s, e);
+}
 void insertionSort(int arr[], int n)
 {
     // let first element is already sorted, so start from 2nd element
@@ -21,6 +110,19 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
 void printingArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -39,7 +141,10 @@ int main()
     {
         cin >> arr[i];
     }
-    insertionSort(arr, n);
+    // insertionSort(arr, n);
+    // selectionSort(arr, n);
+    // mergeSort(arr, 0, n);
+    bubbleSort(arr, n);
     printingArray(arr, n);
 
     return 0;
