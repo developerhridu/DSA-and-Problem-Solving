60b50e04bb40d46ee8ba3bbcd1e80196badb95bd
MR Hridu <developerhridu@gmail.com>
2022-10-20 00:37:58 +0600

    all searching algo

---
+++ GeeksforGeeks/allSearchingAlgo.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, key;
+    cin >> n >> key;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Binary Search --> "
+         << "The Key " << key << " Is Present at Idex " << binarySearch(arr, 0, n, key) << endl;
+
+    return 0;
+}
