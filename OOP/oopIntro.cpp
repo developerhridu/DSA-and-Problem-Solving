ae9e914401fb2b240c08016a44819d8ac0a983a8
MR Hridu <developerhridu@gmail.com>
2022-10-22 23:35:12 +0600

    print detail function added

--- OOP/oopIntro.cpp
+++ OOP/oopIntro.cpp
@@ -7,6 +7,14 @@ public:
     int stdID;
     string department;
     int session;
+
+    void printDetails()
+    {
+        cout << "Student name : " << name << endl;
+        cout << "Student ID : " << stdID << endl;
+        cout << "Department : " << department << endl;
+        cout << "Session : " << session << endl;
+    }
 };
 
 int main()
@@ -16,9 +24,8 @@ int main()
     s1.name = "Mizanur Rahman";
     s1.stdID = 1702153;
     s1.department = "ECE";
-    cout << s1.name << " ";
-    cout << s1.stdID << " ";
-    cout << s1.department << endl;
+    s1.session = 2017;
+    s1.printDetails();
 
     return 0;
 }
