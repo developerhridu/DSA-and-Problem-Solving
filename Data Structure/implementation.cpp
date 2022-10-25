0f3f4c4b8ad59cd091f63fa65aa2faa881445be0
MR Hridu <developerhridu@gmail.com>
2022-10-25 21:03:33 +0600

    Linked List Implementation

---
+++ Data Structure/implementation.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // distructor
+    ~Node()
+    {
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+    }
+};
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void insertAtAnyPosition(Node *&head, Node *&tail, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+void deleteNode(Node *&head, int position)
+{
+
+    // deleting first node
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+
+        // free the memory
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void print(Node *head)
+{
+    if (head == NULL)
+        cout << "Linked List is empty" << endl;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *node1 = new Node(10);
+
+    // head pointed to node1
+    Node *head = node1;
+    Node *tail = node1;
+
+    insertAtHead(head, 5);
+    insertAtHead(head, 0);
+    // print(head);
+
+    insertAtTail(tail, 15);
+    // print(head);
+
+    insertAtAnyPosition(head, tail, 5, 20);
+    // print(head);
+
+    deleteNode(head, 3);
+    print(head);
+
+    return 0;
+}
