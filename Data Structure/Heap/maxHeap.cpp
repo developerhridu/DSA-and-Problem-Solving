ecf7ba6532ff09141286cb01bdac48f6d3568bd6
MR Hridu <developerhridu@gmail.com>
2022-10-30 00:54:15 +0600

    graph implementation using adjacency list

--- Data Structure/Heap/maxHeap.cpp
+++ Data Structure/Heap/maxHeap.cpp
@@ -82,6 +82,32 @@ void Heap:: deleteFromHeap()
 
 }
 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = (2 * i) + 1;
+
+    if(left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if(right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+
+    // if largest != i it mean it largest variable is update so we need to take the pointer to current possiton
+    if(largest != 0)
+    {
+        swap(arr[largest], arr[i]);
+
+        // baki value gula check korbe 
+        heapify(arr, n, largest);
+    }
+}
+
 void Heap:: print()
 {
     for(int i = 1; i <= size; i++)
@@ -101,6 +127,21 @@ int main()
     h.print();
     h.deleteFromHeap();
     h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50}; 
+
+
+    int n = 5;
+    for(int i = n / 2; i >  0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    //printing
+    for(int i =    1; i <= n; i++)
+    {
+        cout << arr[i] << " "; 
+    } cout << endl;
     
 
 
