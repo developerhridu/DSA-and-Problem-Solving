0f6f45312aaab1a1150b95706b48bbeaffa3fbf3
Mizanur Rahman Hridu <developerhridu@gmail.com>
2023-09-18 17:34:25 +0600

    n

--- test.cpp
+++ test.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
 
-void prefixSumArray(int arr[], int n)
-{
-    int sumArr[n];
-    sumArr[0] = arr[0];
-    for (int i = 1; i < n; i++)
-    {
-        sumArr[i] = sumArr[i - 1] + arr[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << sumArr[i] << " ";
-    }
-}
+using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        prefixSumArray(arr, n);
-    }
-
-    return 0;
+    int x, y;
+    cin >> x;
+   cout << "Hello, world!"<< x<< endl;
 }
