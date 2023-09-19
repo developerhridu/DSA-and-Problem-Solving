3e17fee01187b52ebb15a5214a45374e68a1f239
Mizanur Rahman Hridu <developerhridu@gmail.com>
2023-09-19 17:58:20 +0600

    s

--- test.cpp
+++ test.cpp
@@ -1,38 +1,24 @@
-
-// C++ program to implement linear
-// search in unsorted array
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to implement search operation
-int findElement(int arr[], int n, int key)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        if (arr[i] == key)
-            return i;
-     
-      // If the key is not found
-    return -1;
-}
- 
-// Driver's Code
+
 int main()
 {
-    int arr[] = { 12, 34, 10, 6, 40 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    // Using a last element as search element
-    int key = 40;
-   
-      // Function call
-    int position = findElement(arr, n, key);
- 
-    if (position == -1)
-        cout << "Element not found";
-    else
-        cout << "Element Found at Position: "
-             << position + 1;
- 
-    return 0;
+
+	stack <int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+	while (!s.empty())
+    {
+        /* code */
+        cout << s.top() <<" ";
+        s.pop();
+    }
+    cout << endl;
+
+    s.push(10);
+    if(s.empty()) cout << "Empty" << endl;
+    else cout << "not empty" << " size is "<< s.size() << endl;
+    
 }
