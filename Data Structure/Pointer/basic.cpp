530265cee4dc981dae98a771db1dd18243efa76e
Mizanur Rahman Hridu <developerhridu@gmail.com>
2023-09-23 18:03:44 +0600

    pointer

---
+++ Data Structure/Pointer/basic.cpp
@@ -0,0 +1,29 @@
+// C program to illustrate Pointers
+#include <stdio.h>
+
+void geeks()
+{
+	int var = 10;
+    printf("Value at var = %d \n", var);
+	// declare pointer variable
+	int* ptr;
+
+	// note that data type of ptr and var must be same
+	ptr = &var;
+
+
+    // changing value at the address of var
+    *ptr = 15; 
+
+	// assign the address of a variable to a pointer
+	printf("Value at ptr = %p \n", ptr);
+	printf("Value at var = %d \n", var);
+	printf("Value at *ptr = %d \n", *ptr);
+}
+
+// Driver program
+int main()
+{
+	geeks();
+	return 0;
+}
