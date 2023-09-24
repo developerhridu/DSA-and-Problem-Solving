71de4d9c5b789d6297dc613c0f84cbef83003f07
Mizanur Rahman Hridu <developerhridu@gmail.com>
2023-09-24 11:32:17 +0600

    fffff

--- test.cpp
+++ test.cpp
@@ -1,27 +1,58 @@
-// C program to illustrate Pointers
-#include <stdio.h>
+// C++ program for find the largest
+// three elements in an array
+#include <bits/stdc++.h>
+using namespace std;
 
-void geeks()
+// Function to print three largest elements
+void print3largest(int arr[], int arr_size)
 {
-	int var = 10;
-    printf("Value at var = %d \n", var);
-	// declare pointer variable
-	int* ptr;
+	int first, second, third;
 
-	// note that data type of ptr and var must be same
-	ptr = &var;
+	// There should be atleast three elements
+	if (arr_size < 3)
+	{
+		cout << " Invalid Input ";
+		return;
+	}
 
-    *ptr = 15; 
+	third = first = second = INT_MIN;
+	for(int i = 0; i < arr_size; i++)
+	{
+		
+		// If current element is
+		// greater than first
+		if (arr[i] > first)
+		{
+			third = second;
+			second = first;
+			first = arr[i];
+		}
 
-	// assign the address of a variable to a pointer
-	printf("Value at ptr = %p \n", ptr);
-	printf("Value at var = %d \n", var);
-	printf("Value at *ptr = %d \n", *ptr);
+		// If arr[i] is in between first
+		// and second then update second
+		else if (arr[i] > second && arr[i] != first)
+		{
+			third = second;
+			second = arr[i];
+		}
+
+		else if (arr[i] > third && arr[i] != second && arr[i] != first)
+			third = arr[i];
+	}
+
+	cout << "Three largest elements are "
+		<< first << " " << second << " "
+		<< third << endl;
 }
 
-// Driver program
+// Driver code
 int main()
 {
-	geeks();
+	int arr[] = { 12, 13, 1, 10, 34, 11, 34 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	print3largest(arr, n);
 	return 0;
 }
+
+// This code is contributed by Anjali_Chauhan
