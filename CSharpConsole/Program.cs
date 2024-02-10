65681f6cc6b43e49925f287f56df972992cc7228
Mizanur Rahman Hridu <developerhridu@gmail.com>
2024-02-10 18:58:06 +0600

    C#

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -1,48 +1,18 @@
-﻿
-// Iterative C# program to reverse an 
-// array 
-using System;
+﻿using System;
 
-class GFG
+class Program
 {
-
-    /* Function to reverse arr[] from 
-	start to end*/
-    static void rvereseArray(int[] arr,
-                    int start, int end)
+    static void Main()
     {
-        int temp;
+        int[] originalArray = { 1, 2, 3, 4, 5 };
 
-        while (start < end)
+        // Using inbuilt method in C#
+        Array.Reverse(originalArray);
+
+        // Print the reversed array
+        foreach (int num in originalArray)
         {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+            Console.Write(num + " ");
         }
     }
-
-    /* Utility that prints out an 
-	array on a line */
-    static void printArray(int[] arr,
-                            int size)
-    {
-        for (int i = 0; i < size; i++)
-            Console.Write(arr[i] + " ");
-
-        Console.WriteLine();
-    }
-
-    // Driver function 
-    public static void Main()
-    {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
-        printArray(arr, 6);
-        rvereseArray(arr, 0, 5);
-        Console.Write("Reversed array is \n");
-        printArray(arr, 6);
-    }
 }
-
-// This code is contributed by Sam007 
--- test.ts
+++ test.ts
@@ -1,28 +1,7 @@
-// import * as readlineSync from "readline-sync";
+let originalArray = [1, 2, 3, 4, 5];
 
-// // Taking input from the user
-// let userInput: string = readlineSync.question("Enter something: ");
-// let user: number = parseInt(userInput, 10);
+// Using inbuilt method in JavaScript
+let reversedArray = originalArray.reverse();
 
-// // Displaying the input
-// console.log("You entered:", user);
-let num: number[] = [10, 20, 30, 5, 6, 50];
-let num2: number[] = [5, 30];
-
-// let x = (i) => {};
-
-let y = num.map((i) => {
-  return 2 * i;
-});
-
-let f = num.filter((i) => {
-  return i != 10;
-});
-console.log(f);
-
-console.log("value", y);
-
-let n = num.includes(60);
-let m = num.findIndex((i) => i === 50);
-// let m = num.indexOf(50)
-console.log(m);
+// Print the reversed array
+console.log(reversedArray);
